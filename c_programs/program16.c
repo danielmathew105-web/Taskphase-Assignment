257e48e52bb60b8e8b38a38cbd1081882b3d10e8
DanielMathew <danielmathew105@gmail.com>
2025-12-15 10:54:08 +0530

    Add files via upload

---
+++ c_programs/program16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void compress(char a[50],int index);
+int main(){
+    char a[50];
+    printf("Enter string:");
+    scanf("%s",a);
+    printf("Required output:");
+    compress(a,0);
+    return 0;
+          }
+void compress(char a[50],int i){
+    int c=1;
+    if(a[i]=='\0')//checking end of string
+        return;
+    while(a[i]==a[i+c])//checking if consecutive terms are equal
+        c++;
+    printf("%c%d",a[i],c);
+    compress(a,i+c);//moving onto the next letter
+
+                                }
