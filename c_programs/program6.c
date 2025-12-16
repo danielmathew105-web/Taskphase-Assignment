b56c9101e5f4ba847647bed5dd0f997be50967d1
DanielMathew <danielmathew105@gmail.com>
2025-12-16 13:27:44 +0530

    Improve vowel/consonant counting and add comments
    Refactor vowel and consonant counting logic and improve comments.

--- c_programs/program6.c
+++ c_programs/program6.c
@@ -8,21 +8,24 @@ int main(){
     char x[50];
     printf("Enter string:");
     fgets(x, sizeof(x), stdin);
+    x[strcspn(x,"\n")]='\0';//removing newline  
     n=strlen(x);
     for(i=0;i<n;i++){
-        if(x[i]=='a' || x[i]=='e' || x[i]=='i' || x[i]=='o' || x[i]=='u' || x[i]=='A' || x[i]=='E' || x[i]=='I' || x[i]=='O' || x[i]=='U')
-            v++;
-        else
-            c++;
-                            }
+        if((x[i]>='A'&&x[i]<='Z') || (x[i]>='a'&&x[i]<='z')){   //checking for alphabets
+            if(x[i]=='a' || x[i]=='e' || x[i]=='i' || x[i]=='o' || x[i]=='u' || x[i]=='A' || x[i]=='E' || x[i]=='I' || x[i]=='O' || x[i]=='U')
+                v++;
+            else
+                c++;
+                            }                               
+                    }        
     
     printf("No of vowels and consonants are %d and %d\n",v,c);
     //Scrambling
-    srand(time(NULL));
+    srand(time(NULL));//randomises the scrambling order
     for (i = 0; i < n - 1; i++) {
-        int j = i + rand() % (n - i); 
+        int j = i + rand() % (n - i); //adjusting range so that elements don't get re-shuffled
         char temp = x[i];
-        x[i] = x[j];
+        x[i] = x[j];//swaps current element with a random element
         x[j] = temp;
                                 }
     printf("Scrambled string:\n%s", x);
