edd0160378752f1f9013a104fca17e3d6a6d86d4
DanielMathew <danielmathew105@gmail.com>
2025-12-16 13:19:06 +0530

    Update program4.c

--- c_programs/program4.c
+++ c_programs/program4.c
@@ -5,16 +5,21 @@
 int main(){
     int i;
     char a[50],b[50];
-    printf("Enter original password:");
-    scanf("%s",&a);
-    
+    printf("Enter password:");
+    fgets(a,sizeof(a),stdin);//so that spaces can be included
+    a[strcspn(a, "\n")] = '\0';//removing newline from the end of the password
     for(i=0;i<3;i++){
         printf("Confirm password:");
-        scanf("%s",&b);
+        fgets(b,sizeof(b),stdin);//so that spaces can be included
+        b[strcspn(b, "\n")] = '\0';//removing newline from the end of the password
+        
+        if(i==2 && strcmp(a,b)!=0){
+            printf("3 attempts over, exiting");
+            exit(0);}
         if(strcmp(a,b)==0){
             printf("Correct password");
             exit(0);}
-        else
+        else 
             printf("Wrong password, try again\n");
                     }
 return 0;
