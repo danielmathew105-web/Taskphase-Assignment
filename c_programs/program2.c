6e402731c745b7491f65e01ad20110a0a443ea5a
DanielMathew <danielmathew105@gmail.com>
2025-12-16 13:10:09 +0530

    Improve input handling for seconds conversion
    Added input validation for seconds input.

--- c_programs/program2.c
+++ c_programs/program2.c
@@ -1,10 +1,18 @@
 //program that converts seconds into hours, minutes, and remaining seconds
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n,hrs,min,sec,x;
     printf("Enter no of seconds:");
-    scanf("%d",&n);
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input");
+        exit(0);
+                              }
+    if(n<0){
+        printf("Enter postive no of seconds");
+        exit(0);
+           }
     hrs=n/3600;
     min=n%3600;
     x=min/60;
