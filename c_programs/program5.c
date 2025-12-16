9114a81460284491d554e1ca355f160b20c150cc
DanielMathew <danielmathew105@gmail.com>
2025-12-16 13:22:57 +0530

    Update program5.c

--- c_programs/program5.c
+++ c_programs/program5.c
@@ -9,22 +9,27 @@ int main() {
     scanf("%d",&n);
 
     printf("Enter elements:\n");
-    for (i=0;i<n;i++) 
-        scanf("%d", &a[i]);
-    
+    for (i=0;i<n;i++){
+        if (scanf("%d", &a[i]) != 1) {
+        printf("Invalid input");
+        return 0;
+                                  }
+                     }
+        
     for (i=0;i<n;i++) {
         for (j=i+1;j<n;j++) {
             if (a[i]==a[j]) {
                 for (k=j;k<n-1;k++) 
                     a[k]=a[k + 1];
                                             
-                n--;      
-                j--;              }  
-                                    }
+                n--;      //reducing array size
+                j--;        }  //checking shifted elements 
                             }
+                      }
     printf("Array after removing duplicates:\n");
     for (i=0;i<n;i++) {
         printf("%d\t",a[i]);
     }
     return 0;
 }
+
