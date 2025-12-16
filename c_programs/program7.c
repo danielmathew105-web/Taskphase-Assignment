0b7c17b76e9edd284136c6f05c38b1d153afce11
DanielMathew <danielmathew105@gmail.com>
2025-12-16 13:39:45 +0530

    Update program7.c

--- c_programs/program7.c
+++ c_programs/program7.c
@@ -1,5 +1,7 @@
 //Program to find out second largest element
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 int main(){
     int n,i,l,sl;
     int a[50],temp;
@@ -8,21 +10,24 @@ int main(){
     scanf("%d",&n);
     printf("Enter elements:\n");
     for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
+        if(scanf("%d",&a[i])!=1){    //checking for invalid type entered
+            printf("Invalid type");
+            exit(0);            }
     
     l = *p;           // arr[0]
-    sl = 0;     // very small number
+    sl = INT_MIN;     // smallest integer possible
 
     for(i=1;i<n;i++){
         if (*(p+i) > l) {
             sl = l;
             l = *(p+i);
                         }
-        else if (*(p+i) > sl) {
+        else if (*(p+i) > sl && *(p+i)<l) { //ensuring duplication doesn't affect output
             sl = *(p+i);
                               }
                     }
             
+       
     printf("2nd largest=%d",sl);
     return 0;
     
