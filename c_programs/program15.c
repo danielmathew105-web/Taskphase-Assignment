cd9b5f14579fffd10143fc6ae121d7bf58b8524b
DanielMathew <danielmathew105@gmail.com>
2025-12-17 07:02:41 +0530

    Update program15.c

--- c_programs/program15.c
+++ c_programs/program15.c
@@ -1,10 +1,15 @@
 //program to print nth term of fibonacci series
 #include <stdio.h>
+#include <stdlib.h>
 int fib(int n,int a,int b);//function prototype
 int main(){
     int n;
     printf("Enter positive no of terms:");
     scanf("%d",&n);
+    if(n<0){
+        printf("Enter a postive number");
+        exit(0);
+           }
     printf("%dth term of the fibonacci series=%d",n,fib(n-1,0,1));
     return 0;
           }
@@ -15,3 +20,4 @@ int fib(int x,int a,int b){//defining function
     c=a+b;
     return(fib(x-1,b,c));
                         }
+
