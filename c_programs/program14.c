3ac0a8de3ff13d2712137694b217c013ed065a3a
DanielMathew <danielmathew105@gmail.com>
2025-12-17 07:01:37 +0530

    Update program14.c

--- c_programs/program14.c
+++ c_programs/program14.c
@@ -1,9 +1,9 @@
-//program to print line,word,character count of a given string
+//program to print word,line,character count from a file
 #include <stdio.h>
 int main(){
     FILE *f;
-    char ch;
-    int i,wc=1,lc=0,cc=0;
+    char ch, prev=' ';
+    int i,wc=0,lc=0,cc=0;
     f=fopen("input.txt","r");
     if(f==NULL){
         printf("CAN'T OPEN THE FILE");//Checks if fopen() opened the file or not
@@ -12,11 +12,14 @@ int main(){
         cc++;//character count increase
         if(ch=='\n')
             lc++;//line count increase
-        if(ch==' ' || ch=='\n')
-            wc++;//word count increase
+        if(ch!=' ' && ch!='\n' && ch!='\t'){        //checking if the character is a seperator
+            if(prev==' ' || prev=='\n' || prev=='\t')   //checking if the character before ch was a seperator
+                wc++;//word count increase
+                                           }
+                prev=ch;
                              }
     printf("Character count=%d\n",cc);
     printf("Word count=%d\n",wc);
     printf("Line count=%d\n",lc);
 return 0;
-}
+                            }
