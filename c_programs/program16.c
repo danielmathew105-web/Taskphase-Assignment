516798c7fa6f967d2cabce037e8329b027106f77
DanielMathew <danielmathew105@gmail.com>
2025-12-17 08:45:58 +0530

    Update program16.c

--- c_programs/program16.c
+++ c_programs/program16.c
@@ -1,21 +1,32 @@
 //program to Implement a recursive function to compress a string by replacing consecutive duplicate characters with their count
 #include <stdio.h>
-void compress(char a[50],int index);
+void frequency(char a[50],int index);
 int main(){
     char a[50];
     printf("Enter string:");
-    scanf("%s",a);
+    fgets(a,sizeof(a),stdin);
     printf("Required output:");
-    compress(a,0);
+    frequency(a,0);
     return 0;
           }
-void compress(char a[50],int i){
-    int c=1;
+void frequency(char a[50],int i){
+    int j,c=0;
     if(a[i]=='\0')//checking end of string
         return;
-    while(a[i]==a[i+c])//checking if consecutive terms are equal
-        c++;
-    printf("%c%d",a[i],c);
-    compress(a,i+c);//moving onto the next letter
+    if(a[i]==' ' || a[i]=='\n'){
+        frequency(a,i+1);   //ignoring seperators
+        return;                }
 
-                                }
+    for(j=0;j<i;j++)   //checking if character which is already processed is duplicated
+        if(a[j]==a[i])
+            return frequency(a,i+1);    //moving onto next character if character was already processed earlier
+                    
+    for(j=0;a[j]!='\0';j++){    //checking duplication
+        if(a[j]==a[i])
+            c++;           }
+    if(c>1)
+        printf("%c%d",a[i],c);
+    else
+        printf("%c",a[i]);
+    frequency(a,i+1);   //recursive call
+}
